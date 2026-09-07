dc4905930dc16ab4d9b7eecb93d424eeaa97b679
ztawfik523-lgtm <ztawfik523@gmail.com>
2026-09-07 07:09:26 +0300

    test: check live CC:T registry after server start

--- src/main/java/dev/ztawfik/cctweakedhighaudio/HighAudio.java
+++ src/main/java/dev/ztawfik/cctweakedhighaudio/HighAudio.java
@@ -5,6 +5,8 @@ import dan200.computercraft.api.ComputerCraftAPI;
 import dev.ztawfik.cctweakedhighaudio.integration.cct.GenericSourceSelfCheck;
 import dev.ztawfik.cctweakedhighaudio.integration.cct.SpeakerGenericSource;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartedEvent;
 import org.slf4j.Logger;
 
 @Mod(HighAudio.MOD_ID)
@@ -12,14 +14,20 @@ public final class HighAudio {
     public static final String MOD_ID = "cctweakedhighaudio";
     public static final Logger LOGGER = LogUtils.getLogger();
 
+    private final SpeakerGenericSource speakerSource = new SpeakerGenericSource();
+
     public HighAudio() {
         LOGGER.info("CC:Tweaked HighAudio bootstrap loading MILESTONE-001 / EXP-001 GenericSource probe");
 
-        var source = new SpeakerGenericSource();
-        GenericSourceSelfCheck.verify(source);
-        ComputerCraftAPI.registerGenericSource(source);
+        GenericSourceSelfCheck.verify(speakerSource);
+        ComputerCraftAPI.registerGenericSource(speakerSource);
+        NeoForge.EVENT_BUS.addListener(this::onServerStarted);
 
-        LOGGER.info("[EXP-001] GenericSource registered id={}", source.id());
+        LOGGER.info("[EXP-001] GenericSource registered id={}", speakerSource.id());
         LOGGER.info("CC:Tweaked HighAudio bootstrap loaded MILESTONE-001 / EXP-001 GenericSource probe");
+    }
+
+    private void onServerStarted(ServerStartedEvent event) {
+        GenericSourceSelfCheck.verifyLiveServerContext(event.getServer(), speakerSource);
     }
 }
